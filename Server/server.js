21be71f024beb27c85db8784af2fded708ba6317
Abhishek <abhisheksonparote6@gmail.com>
2026-08-24 02:29:36 +0530

    Fix AddFood import case

--- Server/server.js
+++ Server/server.js
@@ -13,10 +13,13 @@ app.use(express.json());
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,  
-    port: process.env.DB_PORT
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 async function testDatabase(){
